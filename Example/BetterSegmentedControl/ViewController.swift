6050f0444332afea010b5f7837e1006bb3964552
George Marmaridis <gmarmas@gmail.com>
2017-10-12 20:47:27 +0200

    Updated example

--- Example/BetterSegmentedControl/ViewController.swift
+++ Example/BetterSegmentedControl/ViewController.swift
@@ -37,18 +37,16 @@ class ViewController: UIViewController {
                                                   normalFont: UIFont(name: "HelveticaNeue-Medium", size: 13.0)!,
                                                   selectedFont: UIFont(name: "HelveticaNeue-Medium", size: 13.0)!)
         // Control 2: Exclusively defined in IB
-        /*
+        
         // Control 3: Many options & error handling
-        control3.titles = ["One","Two","Three","Four","Five","Six"]
-        control3.titleFont = UIFont(name: "HelveticaNeue-Light", size: 14.0)!
-        control3.selectedTitleFont = UIFont(name: "HelveticaNeue-Medium", size: 14.0)!
+        control3.segments = LabelSegment.segments(withTitles: ["One","Two","Three","Four","Five","Six"],
+                                                  normalFont: UIFont(name: "HelveticaNeue-Light", size: 14.0)!,
+                                                  selectedFont: UIFont(name: "HelveticaNeue-Medium", size: 14.0)!)
         do {
             try control3.setIndex(10, animated: false)
-        }
-        catch BetterSegmentedControl.IndexError.indexBeyondBounds(let invalidIndex) {
+        } catch BetterSegmentedControl.IndexError.indexBeyondBounds(let invalidIndex) {
             print("Tried setting invalid index \(invalidIndex) to demonstrate error handling.")
-        }
-        catch {
+        } catch {
             print("An error occured")
         }
         try! control3.setIndex(2, animated: false)
@@ -56,37 +54,18 @@ class ViewController: UIViewController {
         // Control 4: Added as a subview
         let viewSegmentedControl = BetterSegmentedControl(
             frame: CGRect(x: 0.0, y: 332.0, width: view.bounds.width, height: 50.0),
-            titles: ["Artists", "Albums"],
+            segments: LabelSegment.segments(withTitles: ["Artists", "Albums"],
+                                            normalFont: UIFont(name: "HelveticaNeue", size: 16.0)!,
+                                            normalTextColor: .white,
+                                            selectedFont: UIFont(name: "HelveticaNeue", size: 16.0)!,
+                                            selectedTextColor: UIColor(red:0.97, green:0.00, blue:0.24, alpha:1.00)),
             index: 1,
             options: [.backgroundColor(UIColor(red:0.11, green:0.12, blue:0.13, alpha:1.00)),
-                      .titleColor(.white),
                       .indicatorViewBackgroundColor(UIColor(red:0.11, green:0.12, blue:0.13, alpha:1.00)),
-                      .selectedTitleColor(UIColor(red:0.97, green:0.00, blue:0.24, alpha:1.00)),
                       .cornerRadius(0.0),
-                      .titleFont(UIFont(name: "HelveticaNeue", size: 16.0)!),
-                      .selectedTitleFont(UIFont(name: "HelveticaNeue", size: 16.0)!),
                       .bouncesOnChange(false),
                       .panningDisabled(true)])
         view.addSubview(viewSegmentedControl)
-
-        // Control 5: Adding custom subview to Indicator
-        let indicatorControl = BetterSegmentedControl(
-            frame: CGRect(x: 0.0, y: 405.0, width: view.bounds.width, height: 50.0),
-            titles: ["Hello", "Goodbye"],
-            index: 0, options: [.backgroundColor(.lightGray),
-                                .titleColor(.white),
-                                .indicatorViewBorderColor(.white),
-                                .selectedTitleColor(.black),
-                                .bouncesOnChange(false),
-                                .panningDisabled(false)])
-        indicatorControl.autoresizingMask = [.flexibleWidth]
-        let customSubview = UIView(frame: CGRect(x: 0, y: 40, width: 40, height: 4.0))
-        customSubview.backgroundColor = .blue
-        customSubview.layer.cornerRadius = 2.0
-        customSubview.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin]
-        indicatorControl.addSubviewToIndicator(customSubview)
-        view.addSubview(indicatorControl)
-         */
     }
     
     // MARK: - Action handlers
