bababd24d350080400d3bcd044498cd71a2b0304
Ido <Idomo@users.noreply.github.com>
2018-10-01 13:18:19 +0300

    Fix layer.masksToBounds
    Set `layer.masksToBounds` to true only if `indicatorViewInset` isn't negative to achieve the effect I mentioned in #75

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -130,7 +130,10 @@ import Foundation
     }
     /// The indicator view's inset. Defaults to 2.0
     @IBInspectable public var indicatorViewInset: CGFloat = 2.0 {
-        didSet { setNeedsLayout() }
+        didSet {
+            setNeedsLayout()
+            layer.masksToBounds = indicatorViewInset >= 0
+        }
     }
     /// The indicator view's border width
     @IBInspectable public var indicatorViewBorderWidth: CGFloat {
