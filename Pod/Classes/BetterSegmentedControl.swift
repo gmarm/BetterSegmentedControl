e89c184552349286c7873cb6485a4ef32333a7c2
Foltányi Kolos <>
2019-10-10 16:18:12 +0200

    Adds segment spacaing

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -100,6 +100,8 @@ import Foundation
                     cornerRadius = value
                 case let .bouncesOnChange(value):
                     bouncesOnChange = value
+                case let .segmentSpacing(value):
+                    segmentSpacing = value
                 }
             }
         }
@@ -173,6 +175,7 @@ import Foundation
     private var selectedSegments: [UIView] { return selectedSegmentsView.subviews }
     private var segmentViews: [UIView] { return normalSegments + selectedSegments }
     private var totalInsetSize: CGFloat { return indicatorViewInset * 2.0 }
+    private var totalSpacings: CGFloat { return segmentSpacing * CGFloat(normalSegmentCount - 1)  }
     private lazy var defaultSegments: [BetterSegmentedControlSegment] = {
         return [LabelSegment(text: "First"), LabelSegment(text: "Second")]
     }()
@@ -327,8 +330,9 @@ import Foundation
     
     // MARK: Helpers
     private func elementFrame(forIndex index: Int) -> CGRect {
-        let elementWidth = (width - totalInsetSize) / CGFloat(normalSegmentCount)
-        let x = CGFloat(isLayoutDirectionRightToLeft ? lastIndex - index : index) * elementWidth
+        let elementWidth = (width - totalInsetSize - totalSpacings) / CGFloat(normalSegmentCount)
+        let spacingOffset =  CGFloat(index) * segmentSpacing
+        let x = CGFloat(isLayoutDirectionRightToLeft ? lastIndex - index : index) * elementWidth + spacingOffset
         return CGRect(x: x + indicatorViewInset,
                       y: indicatorViewInset,
                       width: elementWidth,
