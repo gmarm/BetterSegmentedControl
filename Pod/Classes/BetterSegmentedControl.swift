5c4c9cebae90cf6a90616989a20e93b0698a80b1
George Marmaridis <gmarmas@gmail.com>
2020-10-18 13:20:07 +0200

    Cleanup and simplify internal implementation

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -37,12 +37,14 @@ import UIKit
         
     // MARK: Properties
     // Public
+    
     /// The selected index. Use `setIndex()` for setting the index.
     public private(set) var index: Int
+    
     /// The segments available for selection.
     public var segments: [BetterSegmentedControlSegment] {
         didSet {
-            applySegments()
+            update()
         }
     }
     
@@ -51,46 +53,51 @@ import UIKit
     
     /// Whether the the control should always send the .valueChanged event, regardless of the index remaining unchanged after interaction. Defaults to `false`.
     @IBInspectable public var alwaysAnnouncesValue: Bool = false
+    
     /// Whether to send the .valueChanged event immediately or wait for animations to complete. Defaults to `true`.
     @IBInspectable public var announcesValueImmediately: Bool = true
+    
     /// Whether the the control should ignore pan gestures. Defaults to `false`.
     @IBInspectable public var panningDisabled: Bool = false
+    
     /// The control's and indicator's corner radii.
     @IBInspectable public var cornerRadius: CGFloat {
         get {
-            return layer.cornerRadius
+            layer.cornerRadius
         }
         set {
             layer.cornerRadius = newValue
-            updateCornerRadii()
+            update()
         }
     }
     
     /// The indicator view's background color.
     @IBInspectable public var indicatorViewBackgroundColor: UIColor? {
         get {
-            return indicatorView.backgroundColor
+            indicatorView.backgroundColor
         }
         set {
             indicatorView.backgroundColor = newValue
         }
     }
+    
     /// The indicator view's inset. Defaults to `2.0`.
     @IBInspectable public var indicatorViewInset: CGFloat = 2.0 {
         didSet {
-            updateCornerRadii()
-            setNeedsLayout()
+            update()
         }
     }
+    
     /// The indicator view's border width.
     @IBInspectable public var indicatorViewBorderWidth: CGFloat {
         get {
-            return indicatorView.layer.borderWidth
+            indicatorView.layer.borderWidth
         }
         set {
             indicatorView.layer.borderWidth = newValue
         }
     }
+    
     /// The indicator view's border color.
     @IBInspectable public var indicatorViewBorderColor: UIColor? {
         get {
@@ -103,8 +110,10 @@ import UIKit
             indicatorView.layer.borderColor = newValue?.cgColor
         }
     }
+    
     /// The duration of the animation of an index change. Defaults to `0.3`.
     @IBInspectable public var animationDuration: TimeInterval = 0.3
+    
     /// The spring damping ratio of the animation of an index change. Defaults to `0.75`. Set to `1.0` for a no bounce effect.
     @IBInspectable public var animationSpringDamping: CGFloat = 0.75
     
@@ -116,24 +125,23 @@ import UIKit
     private var tapGestureRecognizer: UITapGestureRecognizer!
     private var panGestureRecognizer: UIPanGestureRecognizer!
     
-    private var width: CGFloat { return bounds.width }
-    private var height: CGFloat { return bounds.height }
-    private var normalSegmentCount: Int { return normalSegmentsView.subviews.count }
-    private var normalSegments: [UIView] { return normalSegmentsView.subviews }
-    private var selectedSegments: [UIView] { return selectedSegmentsView.subviews }
-    private var segmentViews: [UIView] { return normalSegments + selectedSegments }
-    private var totalInsetSize: CGFloat { return indicatorViewInset * 2.0 }
-    private static var defaultOptions: [Option] = [.backgroundColor(.appleSegmentedControlDefaultControlBackground),
-                                                                         .indicatorViewBackgroundColor(.appleSegmentedControlDefaultIndicatorBackground)]
+    private var width: CGFloat { bounds.width }
+    private var height: CGFloat { bounds.height }
+    private var normalSegmentCount: Int { normalSegmentsView.subviews.count }
+    private var normalSegments: [UIView] { normalSegmentsView.subviews }
+    private var selectedSegments: [UIView] { selectedSegmentsView.subviews }
+    private var segmentViews: [UIView] { normalSegments + selectedSegments }
+    private var lastIndex: Int { segments.endIndex - 1 }
+    private var totalInsetSize: CGFloat { indicatorViewInset * 2.0 }
     private var isLayoutDirectionRightToLeft: Bool {
         let layoutDirection = UIView.userInterfaceLayoutDirection(for: semanticContentAttribute)
         return layoutDirection == .rightToLeft
     }
-    private var lastIndex: Int {
-        return segments.endIndex - 1
-    }
+    private static var defaultOptions: [Option] = [.backgroundColor(.appleSegmentedControlDefaultControlBackground),
+                                                   .indicatorViewBackgroundColor(.appleSegmentedControlDefaultIndicatorBackground)]
     
-    // MARK: Lifecycle
+    // MARK: Initialization
+    
     /// Initializes a new `BetterSegmentedControl` with the parameters passed.
     ///
     /// - Parameters:
@@ -162,6 +170,7 @@ import UIKit
             setOptions(options)
         }
     }
+    
     required public init?(coder aDecoder: NSCoder) {
         self.index = 0
         self.segments = [LabelSegment(text: "First"), LabelSegment(text: "Second")]
@@ -170,34 +179,45 @@ import UIKit
         
         completeInit()
     }
+    
     @available(*, unavailable, message: "Use init(frame:segments:index:options:) instead.")
     convenience override public init(frame: CGRect) {
         self.init(frame: frame, segments: [LabelSegment(text: "First"), LabelSegment(text: "Second")])
     }
+    
     @available(*, unavailable, message: "Use init(frame:segments:index:options:) instead.")
     convenience init() {
         self.init(frame: .zero, segments: [LabelSegment(text: "First"), LabelSegment(text: "Second")])
     }
+    
     private func completeInit() {
         layer.masksToBounds = true
         
+        // 1. add normalSegmentsView
         normalSegmentsView.clipsToBounds = true
         addSubview(normalSegmentsView)
+        
+        // 2. add indicatorView
         addSubview(indicatorView)
         selectedSegmentsView.clipsToBounds = true
+        
+        // 3. add selectedSegmentsView
         addSubview(selectedSegmentsView)
         selectedSegmentsView.layer.mask = indicatorView.segmentMaskView.layer
         
+        // configure gestures
         tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
         addGestureRecognizer(tapGestureRecognizer)
+        
         panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned(_:)))
         panGestureRecognizer.delegate = self
         addGestureRecognizer(panGestureRecognizer)
         
-        applySegments()
-        
-        setNeedsLayout()
+        update()
     }
+    
+    // MARK: View lifecycle
+    
     override open func layoutSubviews() {
         super.layoutSubviews()
         guard normalSegmentCount > 1 else {
@@ -207,22 +227,25 @@ import UIKit
         normalSegmentsView.frame = bounds
         selectedSegmentsView.frame = bounds
         
-        indicatorView.frame = elementFrame(forIndex: index)
+        indicatorView.frame = frameForElement(atIndex: index)
         
         for index in 0...normalSegmentCount-1 {
-            let frame = elementFrame(forIndex: index)
+            let frame = frameForElement(atIndex: index)
             normalSegmentsView.subviews[index].frame = frame
             selectedSegmentsView.subviews[index].frame = frame
         }
     }
-    open override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
-        setDefaultLabelTextSegmentColorsFromInterfaceBuilder()
-    }
+    
     open override func awakeFromNib() {
         super.awakeFromNib()
         setDefaultLabelTextSegmentColorsFromInterfaceBuilder()
     }
+    
+    open override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setDefaultLabelTextSegmentColorsFromInterfaceBuilder()
+    }
+    
     private func setDefaultLabelTextSegmentColorsFromInterfaceBuilder() {
         guard let normalLabelSegments = normalSegments as? [UILabel],
             let selectedLabelSegments = selectedSegments as? [UILabel] else {
@@ -248,8 +271,22 @@ import UIKit
         
         let oldIndex = self.index
         self.index = index
-        moveIndicatorViewToIndex(animated, shouldSendEvent: (self.index != oldIndex || alwaysAnnouncesValue))
+        
+        let shouldSendEvent = (index != oldIndex || alwaysAnnouncesValue)
+        if announcesValueImmediately && shouldSendEvent {
+            sendActions(for: .valueChanged)
+        }
+        
+        moveIndicatorViewToIndex(animated: animated, completion: { [weak self] in
+            guard let weakSelf = self else { return }
+            
+            if !weakSelf.announcesValueImmediately && shouldSendEvent {
+                weakSelf.sendActions(for: .valueChanged)
+            }
+        })
     }
+    
+    // MARK: Option Setting
     /// A list of options to configure the control with.
     public func setOptions(_ options: [Option]) {
         for option in options {
@@ -281,48 +318,47 @@ import UIKit
     }
     
     // MARK: Animations
-    private func moveIndicatorViewToIndex(_ animated: Bool, shouldSendEvent: Bool) {
-        if animated {
-            if shouldSendEvent && announcesValueImmediately {
-                sendActions(for: .valueChanged)
-            }
-            UIView.animate(withDuration: animationDuration,
-                           delay: 0.0,
-                           usingSpringWithDamping: animationSpringDamping,
-                           initialSpringVelocity: 0.0,
-                           options: [.beginFromCurrentState, .curveEaseOut],
-                           animations: { () -> Void in
-                            self.moveIndicatorView()
-            }, completion: { finished -> Void in
-                if finished && shouldSendEvent && !self.announcesValueImmediately {
-                    self.sendActions(for: .valueChanged)
-                }
-            })
-        } else {
-            moveIndicatorView()
-
-            if shouldSendEvent {
-                sendActions(for: .valueChanged)
-            }
-        }
+    private func moveIndicatorViewToIndex(animated: Bool, completion: @escaping () -> Void) {
+        UIView.animate(withDuration: animated ? animationDuration : 0.0,
+                       delay: 0.0,
+                       usingSpringWithDamping: animationSpringDamping,
+                       initialSpringVelocity: 0.0,
+                       options: [.beginFromCurrentState, .curveEaseOut],
+                       animations: { () -> Void in
+                        self.indicatorView.frame = self.normalSegments[self.index].frame
+                        self.layoutIfNeeded()
+        }, completion: { finished -> Void in
+            completion()
+        })
     }
     
     // MARK: Helpers
-    private func applySegments() {
-        normalSegmentsView.subviews.forEach({ $0.removeFromSuperview() })
-        selectedSegmentsView.subviews.forEach({ $0.removeFromSuperview() })
-        
-        for segment in segments {
-            segment.normalView.clipsToBounds = true
-            segment.selectedView.clipsToBounds = true
+    /// Updates the control and triggers a layout refresh.
+    private func update() {
+        func updateSegments() {
+            normalSegmentsView.subviews.forEach({ $0.removeFromSuperview() })
+            selectedSegmentsView.subviews.forEach({ $0.removeFromSuperview() })
             
-            normalSegmentsView.addSubview(segment.normalView)
-            selectedSegmentsView.addSubview(segment.selectedView)
+            for segment in segments {
+                segment.normalView.clipsToBounds = true
+                segment.selectedView.clipsToBounds = true
+                
+                normalSegmentsView.addSubview(segment.normalView)
+                selectedSegmentsView.addSubview(segment.selectedView)
+            }
         }
+        func updateCornerRadii() {
+            indicatorView.cornerRadius = cornerRadius - indicatorViewInset
+            segmentViews.forEach { $0.layer.cornerRadius = indicatorView.cornerRadius }
+        }
+        
+        updateSegments()
+        updateCornerRadii()
         
         setNeedsLayout()
     }
-    private func elementFrame(forIndex index: Int) -> CGRect {
+    
+    private func frameForElement(atIndex index: Int) -> CGRect {
         let elementWidth = (width - totalInsetSize) / CGFloat(normalSegmentCount)
         let x = CGFloat(isLayoutDirectionRightToLeft ? lastIndex - index : index) * elementWidth
         
@@ -334,14 +370,6 @@ import UIKit
     private func nearestIndex(toPoint point: CGPoint) -> Int {
         let distances = normalSegments.map { abs(point.x - $0.center.x) }
         return Int(distances.firstIndex(of: distances.min()!)!)
-    }
-    private func moveIndicatorView() {
-        indicatorView.frame = normalSegments[index].frame
-        layoutIfNeeded()
-    }
-    private func updateCornerRadii() {
-        indicatorView.cornerRadius = cornerRadius - indicatorViewInset
-        segmentViews.forEach { $0.layer.cornerRadius = indicatorView.cornerRadius }
     }
     
     // MARK: Action handlers
