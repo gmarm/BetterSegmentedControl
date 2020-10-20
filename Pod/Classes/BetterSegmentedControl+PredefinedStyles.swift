e0d0f575a2e83d88e9f2049de6ed39cc86fa2dc1
George Marmaridis <gmarmas@gmail.com>
2020-10-20 14:55:20 +0200

    Simplify implementation of `appleStyled` extension

--- Pod/Classes/BetterSegmentedControl+PredefinedStyles.swift
+++ Pod/Classes/BetterSegmentedControl+PredefinedStyles.swift
@@ -11,16 +11,8 @@ public extension BetterSegmentedControl {
      class func appleStyled(frame: CGRect, titles: [String]) -> BetterSegmentedControl {
         let control = BetterSegmentedControl(
             frame: frame,
-            segments: LabelSegment.segments(withTitles: titles,
-                                            normalFont: .systemFont(ofSize: 13.0),
-                                            normalTextColor: .appleSegmentedControlDefaultSegmentText,
-                                            selectedFont: .systemFont(ofSize: 13.0, weight: .medium),
-                                            selectedTextColor: .appleSegmentedControlDefaultSegmentText),
-            index: 0,
-            options: [.backgroundColor(.appleSegmentedControlDefaultControlBackground),
-                      .indicatorViewBackgroundColor(.appleSegmentedControlDefaultIndicatorBackground),
-                      .cornerRadius(8),
-                      .indicatorViewInset(2)])
+            segments: LabelSegment.segments(withTitles: titles),
+            options: [.cornerRadius(8)])
         control.indicatorView.layer.shadowColor = UIColor.black.cgColor
         control.indicatorView.layer.shadowOpacity = 0.1
         control.indicatorView.layer.shadowOffset = CGSize(width: 1, height: 1)
